81bae17520519526442bfd248938db497da50b1c
duytran-vio <ducduyvio@gmail.com>
2024-05-30 08:37:46 +0700

    reconfig CORS, resolve handle Pageable error in request

--- dshop_api/src/main/java/com/nashtech/dshop_api/DshopApiApplication.java
+++ dshop_api/src/main/java/com/nashtech/dshop_api/DshopApiApplication.java
@@ -2,10 +2,7 @@ package com.nashtech.dshop_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:3000")
 public class DshopApiApplication {
 
 	public static void main(String[] args) {
--- dshop_api/src/main/java/com/nashtech/dshop_api/security/SecurityConfiguration.java
+++ dshop_api/src/main/java/com/nashtech/dshop_api/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +39,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JWTAuthenticationFilter authFilter) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                     .requestMatchers(HttpMethod.POST, this.baseUrl + "/auth/**").permitAll()
