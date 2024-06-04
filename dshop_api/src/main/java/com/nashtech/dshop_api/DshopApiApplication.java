28221db9f79236be534480a9bb8e96016942003e
duytran-vio <ducduyvio@gmail.com>
2024-06-04 15:33:09 +0700

    enable JPA Auditing

--- dshop_api/src/main/java/com/nashtech/dshop_api/DshopApiApplication.java
+++ dshop_api/src/main/java/com/nashtech/dshop_api/DshopApiApplication.java
@@ -2,7 +2,9 @@ package com.nashtech.dshop_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
+@EnableJpaAuditing
 public class DshopApiApplication {
 
 	public static void main(String[] args) {
